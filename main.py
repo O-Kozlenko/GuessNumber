fda0d5a7ce7c5530b994d5cadc759cf73ed82085
Oleksandr Kozlenko <stupidahhhit@gmail.com>
2025-11-25 11:37:25 +0200

    Виправлено помилки при новій спробі

--- main.py
+++ main.py
@@ -12,17 +12,24 @@ if difficulty=="Easy":
         if user_input.isdigit():
             user_input = int(user_input)
             if user_input == randnum:
-                print("You guessed! GG")
-                print("Your tries:", tries)
-                print("Try again?")
-                print("Yes/No")
-                user_input = input()
-                if user_input == "Yes" or user_input == "yes":
-                    tries = 0
-                    randnum = random.randint(1, 50)
-                    print("What is your guess?(1-50):")
-                elif user_input == "No" or user_input == "no":
-                    break
+                while True:
+                    print("You guessed! GG")
+                    print("Your tries:", tries)
+                    print("Try again?")
+                    print("Yes/No")
+                    user_input = input()
+                    if user_input == "Yes" or user_input == "yes":
+                        tries = 0
+                        randnum = random.randint(1, 50)
+                        print("Difficulty:")
+                        print("Easy/Normal")
+                        difficulty = input()
+                        print("What is your guess?(1-50):")
+
+                    elif user_input == "No" or user_input == "no":
+                        break
+                    else:
+                        print("Type Yes or No")
             elif user_input > 50 or user_input < 1:
                 print("Error 1")
                 print("Try writing number from 1 to 50")
