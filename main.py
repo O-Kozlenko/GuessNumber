02995649eb729d8e29c8a2b48471f5f12e77667f
Oleksandr Kozlenko <stupidahhhit@gmail.com>
2025-11-25 11:01:14 +0200

    Додано рахівник спроб Додан головний геймплей Додано ліміт до вводу користувача Додано зупинку циклу при правильному вводі

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import random
+from pkgutil import resolve_name
+
 randnum = random.randint(1,50)
 user_input = input()
+tries = 0
 while True:
     if user_input.isdigit():
+        if user_input == randnum:
+            print()
+            break
+        elif user_input>50 or user_input<1:
+            print()
+        elif user_input!=randnum:
+            tries+=1
     else:
         print()
