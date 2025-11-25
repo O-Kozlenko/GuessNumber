882095faa14cbb27e0adb0902163e0f18731378e
Oleksandr Kozlenko <stupidahhhit@gmail.com>
2025-11-25 14:00:50 +0200

    Видалено рівні складності

--- main.py
+++ main.py
@@ -2,62 +2,13 @@ import random
 randnum = random.randint(1,50)
 tries = 0
 print("Welcome to GuessNumber!")
-print("Difficulty:")
-print("Easy/Normal")
-difficulty = input()
 print("What is your guess?(1-50):")
-if difficulty=="Easy":
-    while True:
-        user_input = input()
-        if user_input.isdigit():
-            user_input = int(user_input)
-            if user_input == randnum:
-                while True:
-                    print("You guessed! GG")
-                    print("Your tries:", tries)
-                    print("Try again?")
-                    print("Yes/No")
-                    user_input = input()
-                    if user_input == "Yes" or user_input == "yes":
-                        tries = 0
-                        randnum = random.randint(1, 50)
-                        print("Difficulty:")
-                        print("Easy/Normal")
-                        difficulty = input()
-                        print("What is your guess?(1-50):")
-
-                    elif user_input == "No" or user_input == "no":
-                        break
-                    else:
-                        print("Type Yes or No")
-            elif user_input > 50 or user_input < 1:
-                print("Error 1")
-                print("Try writing number from 1 to 50")
-            elif user_input != randnum:
-                if user_input > randnum:
-                    print("Try lower number")
-                elif user_input < randnum:
-                    print("Try higher number")
-                tries += 1
-        else:
-            print("That's not number")
-if difficulty=="Normal":
-    while True:
-        if tries==10:
-            print("You lost!")
-            print("Try again?")
-            print("Yes/No")
-            user_input= input()
-            if user_input == "Yes" or user_input == "yes":
-                tries = 0
-                randnum = random.randint(1, 50)
-                print("What is your guess?(1-50):")
-            elif user_input == "No" or user_input == "no":
-                break
-        user_input = input()
-        if user_input.isdigit():
-            user_input = int(user_input)
-            if user_input == randnum:
+while True:
+    user_input = input()
+    if user_input.isdigit():
+        user_input = int(user_input)
+        if user_input == randnum:
+            while True:
                 print("You guessed! GG")
                 print("Your tries:", tries)
                 print("Try again?")
@@ -66,17 +17,23 @@ if difficulty=="Normal":
                 if user_input == "Yes" or user_input == "yes":
                     tries = 0
                     randnum = random.randint(1, 50)
+                    print("Difficulty:")
+                    print("Easy/Normal")
+                    difficulty = input()
                     print("What is your guess?(1-50):")
+
                 elif user_input == "No" or user_input == "no":
                     break
-            elif user_input > 50 or user_input < 1:
-                print("Error 1")
-                print("Try writing number from 1 to 50")
-            elif user_input != randnum:
-                if user_input > randnum:
-                    print("Try lower number")
-                elif user_input < randnum:
-                    print("Try higher number")
-                tries += 1
-        else:
-            print("That's not number")
+                else:
+                    print("Type Yes or No")
+        elif user_input > 50 or user_input < 1:
+            print("Error 1")
+            print("Try writing number from 1 to 50")
+        elif user_input != randnum:
+            if user_input > randnum:
+                print("Try lower number")
+            elif user_input < randnum:
+                print("Try higher number")
+            tries += 1
+    else:
+        print("That's not number")
